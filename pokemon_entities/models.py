bce7e29e0577d398bb58f9d419a86962aab7cc1f
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-04-19 21:59:59 +0300

    add base app models, add migrations

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# your models here
+
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, null=True)
+    lat = models.FloatField()
+    lon = models.FloatField()
+    appeared_at = models.DateTimeField(null=True, blank=True)
+    disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(null=True, blank=True)
+    health = models.IntegerField(null=True, blank=True)
+    strength = models.IntegerField(null=True, blank=True)
+    defence = models.IntegerField(null=True, blank=True)
+    stamina = models.IntegerField(null=True, blank=True)
